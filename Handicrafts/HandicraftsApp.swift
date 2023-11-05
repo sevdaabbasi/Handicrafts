b9b6d4df46ebea6bc1ff1f0c4ba109239c4c9cab
Sevda Abbasi <sevdaabbasi37@gmail.com>
2023-11-05 18:32:46 +0300

    Initial Commit

---
+++ Handicrafts/HandicraftsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandicraftsApp.swift
+//  Handicrafts
+//
+//  Created by Sevda Abbasi on 5.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct HandicraftsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
